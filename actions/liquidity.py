cccde540899734cd9ccc463974f509d3ca1f60b8
overcodeme <placmabadei1985@rambler.ru>
2025-06-28 20:44:08 +0300

    liquidity adding completed

--- actions/liquidity.py
+++ actions/liquidity.py
@@ -16,11 +16,12 @@ async def add_liquidity(wallet: Account):
     try:
         contract = w3.eth.contract(address=w3.to_checksum_address(liq_address), abi=zenith_liquidity)
         token1 = random.choice(stables_data)
+        token1_balance = await get_token_balance(wallet.address, token1)
         balance = int(await w3.eth.get_balance(wallet.address))
         amount_in_wei = int(random.randint(settings['TASKS']['LIQUIDITY']['AMOUNT'][0], settings['TASKS']['LIQUIDITY']['AMOUNT'][1]) / 100 * balance)
-        amount1 = int(random.randint(settings['TASKS']['LIQUIDITY']['AMOUNT'][0], settings['TASKS']['LIQUIDITY']['AMOUNT'][1]) / 100 * (await get_token_balance(wallet.address, token1)))
+        amount1 = int(random.randint(settings['TASKS']['LIQUIDITY']['AMOUNT'][0], settings['TASKS']['LIQUIDITY']['AMOUNT'][1]) / 100 * token1_balance)
         logger.info(wallet.address, f'Trying add liquidity PHRS/{token1['name']}')
-        deadline = int(time.time() + 300)
+        deadline = int(time.time() + 1000)
 
         mint_data = contract.encode_abi(
             abi_element_identifier='mint',
@@ -41,26 +42,32 @@ async def add_liquidity(wallet: Account):
 
         refund_data = '0x12210e8a'
 
-        estimate_gas = await contract.functions.multicall([mint_data, refund_data]).estimate_gas({
+        # estimate_gas = await contract.functions.multicall([mint_data, refund_data]).estimate_gas({
+        #     'chainId': 688688,
+        #     'from': wallet.address,
+        #     'gas': random.randint(400000, 600000),
+        #     'gasPrice': await w3.eth.gas_price,
+        #     'nonce': await w3.eth.get_transaction_count(wallet.address),
+        #     'value': amount_in_wei
+        # })
+        # print(estimate_gas)
+
+        data = [mint_data, refund_data]
+
+        tx = await contract.functions.multicall(data).build_transaction({
             'chainId': 688688,
             'from': wallet.address,
-            'value': amount_in_wei,
+            'gas': int(random.randint(300000, 450000) * 1.5),
+            'gasPrice': await w3.eth.gas_price,
             'nonce': await w3.eth.get_transaction_count(wallet.address),
-            'gasPrice': await w3.eth.gas_price
-        })
-
-        tx = await contract.functions.multicall([mint_data, refund_data]).build_transaction({
-            'from': wallet.address,
-            'nonce': await w3.eth.get_transaction_count(wallet.address),
-            'gas': estimate_gas * 2,
-            'gasPrice': int(await w3.eth.gas_price * 1.2)
+            'value': amount_in_wei
         })
 
         signed_tx = wallet.sign_transaction(tx)
         tx_hash = await w3.eth.send_raw_transaction(signed_tx.raw_transaction)
 
         tx_receipt = await w3.eth.wait_for_transaction_receipt(tx_hash)
-        if tx_receipt.status == 200:
+        if tx_receipt.status == 1:
             logger.success(wallet.address, f'Successfully added PHRS/{token1['name']} liqudity')
             return True
         else:
